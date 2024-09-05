5d7d066f177c2f508239abdd8c760cbd56583cab
yasweh <myayyas@gmail.com>
2024-09-05 13:19:07 +0700

    added tool type

--- main/views.py
+++ main/views.py
@@ -4,7 +4,8 @@ def show_main(request):
     context = {
         'Name' : 'Daffa Abyaz',
         'Price': '30k Dollars',
-        'Description': 'Strong worker, could handle any crop farm.'
+        'Description': 'Strong worker, could handle any crop farm.',
+        'Type': 'Living Tool'
     }
 
     return render(request, "main.html", context)
